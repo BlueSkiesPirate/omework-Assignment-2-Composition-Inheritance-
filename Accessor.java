b937aded0a2d34fd5c149e87c285f5044551a999
Johny Nolasco <johnynolasco@Johnys-Air-2.home>
2025-03-13 19:28:58 -0400

    Added AccessManagegentTool class and controlled access to departments and courses setters and getters

--- Accessor.java
+++ Accessor.java
@@ -7,155 +7,173 @@ Assignment nameHomework Assignment #2: Composition & Inheritance (*GROUP WORK*)
  */
 
 public class Accessor {
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//------------------This is where we create the professor objects
-    Professor p1 = new Professor(  
-    "Donald", 
-    75, 
-    "123-45-6789", 
-    true,  
-    "4902004", 
-    120000, 
-    41,
-    "Computer science",
-    "AI",
-    "1",
-    true);
-
-    Professor p2 = new Professor(
-        "Jimmy", 
-        30, 
-        "111-11-1111", 
+    //------------------This is where we create the professor objects
+        Professor p1 = new Professor(  
+        "Donald", 
+        75, 
+        "123-45-6789", 
         true,  
-        "658323", 
-        75000, 
-        2,
-        "Math",
-        "Riemman sums",
-        "220",
-        false
-    );
+        "4902004", 
+        120000, 
+        41,
+        "Computer science",
+        "AI",
+        "1",
+        true);
 
-    Professor p3 = new Professor(
-    "Rick", 
-    50, 
-    "123-47-1111", 
-    true,  
-    "583022", 
-    100000, 
-    32,
-    "Chemisrty",
-    "Cellular decoding",
-    "7",
-    true
-    );
+        Professor p2 = new Professor(
+            "Jimmy", 
+            30, 
+            "111-11-1111", 
+            true,  
+            "658323", 
+            75000, 
+            2,
+            "Math",
+            "Riemman sums",
+            "220",
+            false
+        );
 
-    Professor p4 = new Professor(
-        "Sarah", 
-        45, 
-        "222-22-2222", 
+        Professor p3 = new Professor(
+        "Rick", 
+        50, 
+        "123-47-1111", 
         true,  
-        "987654", 
-        95000, 
-        20,
-        "Physics",
-        "Quantum Mechanics",
-        "15",
+        "583022", 
+        100000, 
+        32,
+        "Chemisrty",
+        "Cellular decoding",
+        "7",
         true
-    );
-//----------------------This is where we create the student objects
-    College_student s1 = new College_student(
-        "Izzy" , 
-        19, 
-        "422-74-5829", 
-        true, 
-        "782945", 
-        3.9, 
-        "Computer Science", 
-        "A");
+        );
 
-    College_student s2 = new College_student(
-        "Bobby" , 
-        20, 
-        "333-33-3333", 
-        true, 
-        "124829", 
-        3.8, 
-        "Computer Engineering", 
-        "A-");
+        Professor p4 = new Professor(
+            "Sarah", 
+            45, 
+            "222-22-2222", 
+            true,  
+            "987654", 
+            95000, 
+            20,
+            "Physics",
+            "Quantum Mechanics",
+            "15",
+            true
+        );
+    //----------------------This is where we create the student objects
+        College_student s1 = new College_student(
+            "Izzy" , 
+            19, 
+            "422-74-5829", 
+            true, 
+            "782945", 
+            3.9, 
+            "Computer Science", 
+            "A");
 
-    College_student s3 = new College_student(
-        "Mike" , 
-        20, 
-        "748-83-7247", 
-        true, 
-        "81983", 
-        2.0, 
-        "Physics", 
-        "C");
+        College_student s2 = new College_student(
+            "Bobby" , 
+            20, 
+            "333-33-3333", 
+            true, 
+            "124829", 
+            3.8, 
+            "Computer Engineering", 
+            "A-");
 
-    College_student s4 = new College_student(
-        "Alex" , 
-        21, 
-        "425-86-8392", 
-        true, 
-        "839822", 
-        3.5, 
-        "Bioengineering", 
-        "B+");
+        College_student s3 = new College_student(
+            "Mike" , 
+            20, 
+            "748-83-7247", 
+            true, 
+            "81983", 
+            2.0, 
+            "Physics", 
+            "C");
 
-  //This is where we create the rosters and lists of faculty
+        College_student s4 = new College_student(
+            "Alex" , 
+            21, 
+            "425-86-8392", 
+            true, 
+            "839822", 
+            3.5, 
+            "Bioengineering", 
+            "B+");
 
-    College_student[] roster1 = new College_student[2];
-    College_student[] roster2 = new College_student[2];
+    //This is where we create the rosters and lists of faculty
 
-    roster1[0] = s1;
-    roster1[1] = s2;
+        College_student[] roster1 = new College_student[2];
+        College_student[] roster2 = new College_student[2];
 
-    roster2[0] = s3;
-    roster2[1] = s4;
+        roster1[0] = s1;
+        roster1[1] = s2;
 
-    Professor[] facultyList1 = new Professor[2];
-    Professor[] facultyList2 = new Professor[2];
+        roster2[0] = s3;
+        roster2[1] = s4;
 
-    facultyList1[0] = p1;
-    facultyList1[1] = p2;
+        Professor[] facultyList1 = new Professor[2];
+        Professor[] facultyList2 = new Professor[2];
 
-    facultyList2[0] = p3;
-    facultyList2[1] = p4;
+        facultyList1[0] = p1;
+        facultyList1[1] = p2;
+
+        facultyList2[0] = p3;
+        facultyList2[1] = p4;
 
 
-    //This is where we create the courses
+        //This is where we create the courses
 
-    Course c1 = new Course(p1, "Programming 1", roster2);
-    Course c2 = new Course(p2, "Calculus", roster1);
-    Course c3 = new Course(p3, "Chem 1", roster1);
-    Course c4 = new Course(p4, "Physics 1", roster2);
+        Course c1 = new Course(p1, "Programming 1", roster2);
+        Course c2 = new Course(p2, "Calculus", roster1);
+        Course c3 = new Course(p3, "Chem 1", roster1);
+        Course c4 = new Course(p4, "Physics 1", roster2);
+        
+
+        Course[] courseList1 = new Course[2];
+        Course[] courseList2 = new Course[2];
+
+        courseList1[0] = c1;
+        courseList1[1] = c2;
+
+        courseList2[0] = c3;
+        courseList2[1] = c4;
+
+        Department d1 = new Department(p1, facultyList2, courseList1);
+        Department d2 = new Department(p2, facultyList1, courseList2);
+
+        System.out.println(d1.toString());
+        System.out.println(d2.toString());
+
+
+
+        // Extra Credit
+
+        System.out.println("////////////////////////////////////////////////////////////");
+        System.out.println("\nWe will use the course name as an example:");
+        System.out.println("c1 course name using course getter: " + c1.getCourseName());
+        System.out.println("\nProfessor using course getter:");
+        System.out.println(p1.getCourseCourse_name(c1));
+        
+        System.out.println("\nCollege student using course getter:");
+        System.out.println(s1.getCourseCourse_name(c1));
     
 
-    Course[] courseList1 = new Course[2];
-    Course[] courseList2 = new Course[2];
+        System.out.println("\nProfessor using course setter and result:");
+        p1.setCourseCourse_name(c1, "Programming 2");
+        System.out.println(p1.getCourseCourse_name(c1));
 
-    courseList1[0] = c1;
-    courseList1[1] = c2;
+        System.out.println("\nCollege student using course setter and result:");
+        s1.setCourseCourse_name(c1, "Anything 2");
+        
+        System.out.println();
 
-    courseList2[0] = c3;
-    courseList2[1] = c4;
+        System.out.println("Same happens with departments.\n");
 
-    Department d1 = new Department(p1, facultyList2, courseList1);
-    // Department d2 = new Department(p2, facultyList1, courseList2);
-
-    // System.out.print(p1 + "\n\n");
-    // System.out.print(s1 + "\n\n");
-    // System.out.print(c1 + "\n\n") ;
-    System.out.print(d1);
-    // System.out.print(d1 + "\n\n");
- }  
+        System.out.println("////////////////////////////////////////////////////////////");
+    }  
 }
-
-// Extra credit: (10 points)
-
-// To protect the data integrity, you should make it so the instances of professor can
-//  use both the set and get methods of department and course, but the instances of student 
-//  can only use the get methods of department and course.
--- College_student.java
+++ College_student.java
@@ -34,7 +34,59 @@ public class College_student extends Student {
             (c.grade != null && c.grade != "");
     }
 
-    // Getters and setters
+    // Controlled Access Accessors for Department ------------------------------
+    public Professor getDeptChair(Department dept) {
+        return AccessManagementTool.getDeptChair(this, dept);
+    }
+
+    public Professor[] getDeptFaculty(Department dept) {
+        return AccessManagementTool.getDeptFaculty(this, dept);
+    }
+
+    public Course[] getDeptCourses(Department dept) {
+        return AccessManagementTool.getDeptCourses(this, dept);
+    }
+
+    // Constrolled Access Mutators for Department
+    public void setDeptChair(Department dept, Professor chair) {
+        AccessManagementTool.setDeptChair(this, dept, chair);
+    }
+
+    public void setDeptFaculty(Department dept, Professor[] faculty) {
+        AccessManagementTool.setDeptFaculty(this, dept, faculty);
+    }
+
+    public void setDeptCourses(Department dept, Course[] courses) {
+        AccessManagementTool.setDeptCourses(this, dept, courses);
+    }
+
+    // Controlled Access Accessors for Course ------------------------------
+    public Professor getCourseInstructor(Course course) {
+        return AccessManagementTool.getCourseInstructor(this, course);
+    }
+
+    public String getCourseCourse_name(Course course) {
+        return AccessManagementTool.getCourseCourse_name(this, course);
+    }
+
+    public College_student[] getCourseStudents(Course course) {
+        return AccessManagementTool.getCourseStudents(this, course);
+    }
+
+    // Constrolled Access Mutators for Course ------------------------------
+    public void setCourseInstructor(Course course, Professor instructor) {
+        AccessManagementTool.setCourseInstructor(this, course, instructor);
+    }
+
+    public void setCourseCourse_name(Course course, String name) {
+        AccessManagementTool.setCourseCourse_name(this, course, name);
+    }
+
+    public void setCourseStudents(Course course, College_student[] students) {
+        AccessManagementTool.setCourseStudents(this, course, students);
+    }
+
+    // Getters and setters ------------------------------
     public String getMajor() {
         return major;
     }
